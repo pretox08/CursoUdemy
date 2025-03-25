389841baa02f5a2d5f9e9e3e8a49c20f704a3c4e
Pedro <pedropaulocarvalho08@gmail.com>
2025-03-25 11:31:43 -0300

    Add project files.

---
+++ ConsoleAppDotNET/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppDotNET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salve APP .NET!");
+        }
+    }
+}
